e4cd52d13e7ec904f63dac2f0994d9a5caa72881
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-10-29 10:27:03 +0200

    Cleaning the Array (using `forEach` method)

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 // Function to remove duplicate elements from an array.
 function removeDuplicates(arr) {
-	const cleanedArray = [];
+	const nonDuplicatesArr = [];
 
 	arr.forEach(item => {
 		const lowercaseItem = item.toLowerCase();
 
-		if (!cleanedArray.some(checkItem => checkItem.toLowerCase() === lowercaseItem)) {
-			cleanedArray.push(item);
+		if (!nonDuplicatesArr.some(checkDuplicateItem => checkDuplicateItem.toLowerCase() === lowercaseItem)) {
+			nonDuplicatesArr.push(item);
 		}
 	});
 
-	return cleanedArray;
+	return nonDuplicatesArr;
 }
 
 // Example of using the function.
 const arr1 = ['html', 'css', 'html', 'js'];
 const arr2 = ['html', 'css', 'js', 'html', 'js', 'python', 'js', 'scss'];
 const arr3 = ['html', 'css', 'html', 'HTML', 'js']; // Added an uppercase duplicate.
-const cleanedArray1 = removeDuplicates(arr1);
-const cleanedArray2 = removeDuplicates(arr2);
-const cleanedArray3 = removeDuplicates(arr3);
+const cleanArr1 = removeDuplicates(arr1);
+const cleanArr2 = removeDuplicates(arr2);
+const cleanArr3 = removeDuplicates(arr3);
 
 // Outputing the result to the console.
-console.log(`Cleaned Array1:`, cleanedArray1);
-console.log(`Cleaned Array2:`, cleanedArray2);
-console.log(`Cleaned Array3:`, cleanedArray3);
+console.log(`Clean Array arr1:`, cleanArr1);
+console.log(`Clean Array arr2:`, cleanArr2);
+console.log(`Clean Array arr3:`, cleanArr3);
